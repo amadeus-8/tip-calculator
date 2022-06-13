39b5a921a673e58e85851f631d6c11d94a6cb200
ataubaldiyev <ataubaldiyev@beeline.kz>
2022-06-13 22:11:03 +0600

    Added container component and sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Container } from './components/container/Container'
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+export default function App() {
+    return (
+        <Container>
+            <div>test</div>
+        </Container>
+    )
 }
-
-export default App
