80e4253fc15a980043f456ac29de374691d79633
ataubaldiyev <ataubaldiyev@beeline.kz>
2022-06-14 22:14:03 +0600

    Reset button created

---
+++ src/components/reset/Reset.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './Reset.module.scss'
+
+export const Reset = () => {
+    return <div className={styles.wrapper}>Reset</div>
+}
--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -4,19 +4,14 @@ import styles from './Result.module.scss'
 
 import { Card } from '../card/Card'
 import { ResultValue } from '../resultValue/ResultValue'
+import { Reset } from '../reset/Reset'
 
 export const Result = () => {
     return (
         <Card color="cyan">
-            {/*<div className={styles.output}>*/}
-            {/*    <div className={styles.titleWrapper}>*/}
-            {/*        <div className={styles.title}>Tip Amount</div>*/}
-            {/*        <div className={styles.description}>/ person</div>*/}
-            {/*    </div>*/}
-            {/*    <div className={styles.value}>$0.00</div>*/}
-            {/*</div>*/}
             <ResultValue name="Tip Amount" value={0} />
-            <ResultValue name="Tip Amount" value={0} />
+            <ResultValue name="Total" value={0} />
+            <Reset />
         </Card>
     )
 }
